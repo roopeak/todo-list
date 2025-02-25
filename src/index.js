89ea1c2dc92a1093ce44dafdd1385d9f250e08f5
Roope Kärkkäinen <roopeak@protonmail.com>
2025-02-25 19:01:34 +0200

    Refactor module generateTasks.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,4 @@ import './styles.css'
 
 import { loadUI } from './modules/loadUI';
 
-export let tasks = [
-	{
-		title: 'Clean a house',
-		dueDate: 'not set',
-		priority: 'normal',
-	},
-	{
-		title: 'Take out trash',
-		dueDate: '1/Mar/2025',
-		priority: 'low',
-	},
-	{
-		title: 'Study for an exam',
-		dueDate: '5/Mar/2025',
-		priority: 'high',
-	},
-	{
-		title: 'Pay invoices',
-		dueDate: '25/Apr/2025',
-		priority: 'high',
-	},
-	{
-		title: 'Buy groceries',
-		dueDate: '15/Mar/2025',
-		priority: 'low',
-	}
-	
-]
-
 loadUI();
